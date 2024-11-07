aa966b59915cc001117891a99f1ccb46ef270bc3
alanabyan <alanabyan4@gmail.com>
2024-11-07 09:08:35 +0700

    feat: mobile sidebar

--- components/BreadcrumbHeader.tsx
+++ components/BreadcrumbHeader.tsx
@@ -3,12 +3,14 @@
 import { usePathname } from 'next/navigation';
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList } from './ui/breadcrumb';
+import { MobileSidebar } from './Sidebar';
 
 function BreadcrumbHeader() {
     const pathname = usePathname();
     const paths = pathname === '/' ? [''] : pathname?.split('/');
     return (
         <div className="flex items-center flex-start">
+            <MobileSidebar />
             <Breadcrumb>
                 <BreadcrumbList>{
                     paths.map((path, index) => (
